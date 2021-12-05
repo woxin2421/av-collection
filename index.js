0822d2f3acfa5e71b8fdfef15f6b944d7d11287a
kaitim <kaitim0806@gmail.com>
2021-12-05 22:29:05 +0800

    Build mongoose CRUD.

--- index.js
+++ index.js
@@ -1,67 +1,63 @@
-const express = require('express');
-const Joi = require('joi');
-const app = express();
+const mongoose = require('mongoose');
 
-app.use(express.json());
+mongoose.connect('mongodb://localhost/av-collection')
+    .then(() => console.log('COnnected to MongoDB...'))
+    .catch(err => console.error('Could not connect to MongoDB...', err))
 
-const courses = [
-    { id: 1, name: 'course1' },
-    { id: 2, name: 'course2' },
-    { id: 3, name: 'course3' },
-];
+const movieSchema = new mongoose.Schema({
+    productNumber: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 32,
+    },
+    artist: String,
+    tags: [String],
+    releaseDate: Date
+});
 
-app.get('/', (req, res) => {
-    res.send('hello');
-})
+const Movie = mongoose.model('Movie', movieSchema);
 
-app.get('/api/courses', (req, res) => {
-    res.send(courses);
-})
-
-app.get('/api/courses/:id', (req, res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) return res.status(404).send("no course");
-    res.send(course);
-})
-
-app.post('/api/courses', (req, res) => {
-    const { error } = validateCourse(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
-
-    const course = {
-        id: courses.length + 1,
-        name: req.body.name
-    };
-    courses.push(course);
-    res.send(course);
-})
-
-app.put('/api/courses/:id', (req, res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) return res.status(404).send("no course");
-
-    const { error } = validateCourse(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
-
-    course.name = req.body.name;
-    res.send(course);
-})
-
-app.delete('/api/courses/:id', (req, res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) return res.status(404).send("no course");
-
-    const index = courses.indexOf(course);
-    courses.splice(index, 1);
-
-    res.send(course);
-})
-
-function validateCourse(course) {
-    const schema = Joi.object({
-        name: Joi.string().min(3).required()
+async function createMovie() {
+    const movie = new Movie({
+        productNumber: 'SSIS-179',
+        artist: '和知すばる',
     });
-    return schema.validate(course);
+
+    const result = await movie.save();
+    console.log(result);
 }
 
-app.listen(3000, () => console.log('listening to 3000'));
+async function getMovies() {
+    const pageNumber = 1;
+    const pageSize = 10;
+
+    const movies = await Movie
+        .find({ productNumber: 'SSIS-179' })
+        .skip((pageNumber - 1) * pageSize)
+        .limit(10)
+        .sort({ productNumber: 1 })
+        .select({ productNumber: 1, artist: 1, tags: 1, });
+    console.log(movies);
+}
+
+async function updateMovie(id) {
+    const movie = await Movie.findById(id);
+    if (!movie) return;
+
+    movie.tags = ['Big Tits', 'Lotion'];
+
+    const result = await movie.save();
+    console.log(result);
+}
+
+async function removeMovie(id) {
+    const movie = await Movie.findByIdAndRemove(id);
+    console.log(movie);
+}
+
+// createMovie();
+// getMovies();
+// updateMovie('61accc802d6cda33a85b239d');
+// removeMovie('61acc316cba0d02bdf8d401c');
+
